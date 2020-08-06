1215a9880ddda7dd48c44f8616df67d82279254f
김찬민 <rlacksals96@gmail.com>
2020-08-06 17:45:05 +0900

    style: delete unnessasary files

--- repository/IProjectMemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.soda.web.repository;
-
-import com.soda.web.domain.entity.ClubMember;
-import com.soda.web.domain.entity.Project;
-import com.soda.web.domain.entity.ProjectMember;
-
-public interface IProjectMemberRepository {
-    public void registerParticipant(Project project,ClubMember clubMember);
-}
--- repository/IProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.soda.web.repository;
-
-import com.soda.web.domain.entity.Project;
-
-public interface IProjectRepository {
-    public void registerProject(Project project);
-    public void registerRepo(String projectName,String url);
-    public void registerStatus(String projectName,int projectStatus);
-}
