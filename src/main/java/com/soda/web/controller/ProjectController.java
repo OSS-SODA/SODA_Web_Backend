5f31de35f758b694701fe1bba7e9f28d723ec33d
Jihyun <bjih1999@naver.com>
2020-08-06 19:07:09 +0900

    url 재설계 및 프로젝트 생성 객체 추가

--- src/main/java/com/soda/web/controller/ProjectController.java
+++ src/main/java/com/soda/web/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package com.soda.web.controller;
 
 import com.soda.web.domain.entity.Project;
+import com.soda.web.request.CreateProjectRequest;
 import com.soda.web.request.ModifyProjectRequest;
 import com.soda.web.response.LinksResponse;
 import com.soda.web.response.ProjectResponse;
@@ -12,20 +13,20 @@ import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/projects/{id}")
+@RequestMapping(value = "/projects")
 public class ProjectController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Project addProject(
             @PathVariable Long projectId,
-            @RequestBody ModifyProjectRequest addProjectRequest,
+            @RequestBody CreateProjectRequest createProjectRequest,
             HttpServletRequest response
     ) {
         return null;
     }
 
-    @GetMapping
+    @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ProjectResponse getProject(
             @PathVariable Long projectId
@@ -33,7 +34,7 @@ public class ProjectController {
         return null;
     }
 
-    @PutMapping
+    @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public LinksResponse updateProject(
             @PathVariable Long projectId,
@@ -42,7 +43,7 @@ public class ProjectController {
         return null;
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public LinksResponse deleteProject(
             @PathVariable Long ProjectId
